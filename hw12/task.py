bbed30036acb90fec46c8d60dbfe52ed6bc7a651
iYoQ <iyoqpwnz@gmail.com>
2021-04-21 09:12:34 +0300

    fix

--- hw12/task.py
+++ hw12/task.py
@@ -9,14 +9,21 @@ def task_2(tuple_1, tuple_2, tuple_3):
     return filter(lambda x: tmp.count(x) == 1, tmp)
 
 def task_3(tuple_1, tuple_2, tuple_3):
-    tmp = task_1(tuple_1, tuple_2, tuple_3)
-    return filter(lambda x: tuple_1.index(x) == tuple_2.index(x) == tuple_3.index(x), tmp)
+    # tmp = map(set, zip(tuple_1, tuple_2, tuple_3))
+    # return filter(lambda x: len(x) == 1, tmp)
+    tmp = list(zip(tuple_1, tuple_2, tuple_3))
+    return [tmp[i][0] for i, (x, y, z) in enumerate(tmp) if x == y == z]
+    
+    
+
+
 
 def start():
-    tuple_1 = (1, 3, 5, 9, 10, 11)
-    tuple_2 = (3, 4, 6, 9, 10, 1)
-    tuple_3 = (7, 2, 3, 9, 10)
-    print("task 1:", *task_1(tuple_1, tuple_2, tuple_3))
+    tuple_1 = (1, 3, 5, 4, 2, 10, 10, 10, 10)
+    tuple_2 = (4, 3, 7, 4, 1, 10, 10, 10)
+    tuple_3 = (1, 3, 6, 4, 9, 10, 10)
+    # print("zip", set(zip(tuple_1, tuple_2, tuple_3)))
+    print("task 1:", *set(task_1(tuple_1, tuple_2, tuple_3)))
     print("task 2:", *task_2(tuple_1, tuple_2, tuple_3))
     print("task 3:", *task_3(tuple_1, tuple_2, tuple_3))
 
