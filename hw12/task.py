9daebf2ee372e4e435f1151c692bd900e1481d23
iYoQ <iyoqpwnz@gmail.com>
2021-04-25 00:48:08 +0300

    fix

--- hw12/task.py
+++ hw12/task.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from random import randint
+
 def task_1(tuple_1, tuple_2, tuple_3):
-    return filter(lambda x: x in tuple_2 and x in tuple_3, tuple_1)
+    return filter(lambda x: x in tuple_2 and x in tuple_3, set(tuple_1))
 
 
 def task_2(tuple_1, tuple_2, tuple_3):
@@ -12,18 +14,13 @@ def task_3(tuple_1, tuple_2, tuple_3):
     # tmp = map(set, zip(tuple_1, tuple_2, tuple_3))
     # return filter(lambda x: len(x) == 1, tmp)
     tmp = list(zip(tuple_1, tuple_2, tuple_3))
-    return [tmp[i][0] for i, (x, y, z) in enumerate(tmp) if x == y == z]
+    return [x for (x, y, z) in tmp if x == y == z]
     
-    
-
-
-
 def start():
     tuple_1 = (1, 3, 5, 4, 2, 10, 10, 10, 10)
     tuple_2 = (4, 3, 7, 4, 1, 10, 10, 10)
     tuple_3 = (1, 3, 6, 4, 9, 10, 10)
-    # print("zip", set(zip(tuple_1, tuple_2, tuple_3)))
-    print("task 1:", *set(task_1(tuple_1, tuple_2, tuple_3)))
+    print("task 1:", *task_1(tuple_1, tuple_2, tuple_3))
     print("task 2:", *task_2(tuple_1, tuple_2, tuple_3))
     print("task 3:", *task_3(tuple_1, tuple_2, tuple_3))
 
