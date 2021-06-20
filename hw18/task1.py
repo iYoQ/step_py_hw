0ba08c1a3795959131d98ff6452660a06ab0bfb5
iYoQ <iyoqpwnz@gmail.com>
2021-06-20 21:03:25 +0300

    rework

--- hw18/task1.py
+++ hw18/task1.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+from functools import singledispatchmethod
+
+W_INPUT = 'wrong input'
+
 
 class Employee():
     def __init__(self, first:str='test_name', last:str='test_lastName', pay:int=20000):
         self.__first = first
         self.__last = last
         self._pay = pay
-    
+
     @property
     def email(self):
         return f'{self.__first}.{self.__last}@email.com'
@@ -22,30 +26,53 @@ class Employee():
 
     def __str__(self):
         return f'name: {self.__first} last: {self.__last} pay: {self._pay}'
-    
+
     @property
     def fullname(self):
         return f'{self.__first} {self.__last}'
 
-class Director(Employee): 
+class Director(Employee):
     def __init__(self, first:str='test_name', last:str='test_lastName', pay:int=20000):
         super().__init__(first, last, pay)
         self.sub = []
 
     def __str__(self):
-        return f'{self.fullname} \nEmployee: {", ".join(map(lambda x: x.fullname, self.sub))}'
+        return f'{self.fullname} \nEmployee: {", ".join(map(lambda employee: employee.fullname, self.sub))}'
 
-    def hire(self, first, last, position, pay, prog_lang=None):
+    @singledispatchmethod
+    def hire(self, first=None, last=None, position=None, pay=None, prog_lang=None):
+        print(W_INPUT)
+
+    @hire.register
+    def _(self, first:str, last:str, position, pay, prog_lang=None):
         if position == Developer:
             self.sub.append(position(first, last, pay, prog_lang))
-        if position == Support:
+        elif position == Support:
             self.sub.append(position(first, last, pay))
+        else:
+            print(W_INPUT)
 
-    def dismiss(self, first, last):
-        for index, obj in enumerate(self.sub):
+    @hire.register
+    def _(self, obj:Employee):
+        self.sub.append(obj)
+
+    @singledispatchmethod
+    def dismiss(self, first=None, last=None):
+        print(W_INPUT)
+
+    @dismiss.register
+    def _(self, first:str, last:str):
+        for obj in self.sub:
             if obj.fullname == f'{first} {last}':
-                self.sub.pop(index)
+                self.sub.remove(obj)
                 return f'dissmis {obj.fullname}'
+        return 'no found'
+
+    @dismiss.register
+    def _(self, obj:Employee):
+        if obj in self.sub:
+            self.sub.remove(obj)
+            return f'dissmis {obj.fullname}'
         return 'no found'
 
 class Developer(Employee):
@@ -79,10 +106,29 @@ class Support(Employee):
 
 
 if __name__ == '__main__':
-    director = Director('Jane', 'Doe')
-    director.hire('John', 'Doe', Developer, 40000, 'py')
-    print(director)
-
-    director.dismiss('John', 'Doe')
-    print(director)
     
+    def test_hire_and_dismiss_by_name():
+        director = Director('Jane', 'Doe')
+        director.hire('John', 'Doe', Developer, 40000, 'c')
+
+        print(director)
+
+        director.dismiss('John', 'Doe')
+        print(director)
+
+
+
+    def test_hire_and_dismiss_by_obj():
+        director = Director('Jane', 'Doe')
+        support = Support()
+
+        
+        director.hire(support)
+        print(director)
+        
+        director.dismiss(support)
+        print(director)
+
+
+    test_hire_and_dismiss_by_name()
+    test_hire_and_dismiss_by_obj()
